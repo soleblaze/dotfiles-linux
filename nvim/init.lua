968663b91cc705524e9766d5bc475d631f62c03e
Soleblaze <soleblaze@skyshadows.net>
2022-12-22 01:39:34 -0600

    Add other vim files

--- nvim/init.lua
+++ nvim/init.lua
@@ -17,4 +17,7 @@ require("toggle-autoformat")
 require("toggle-cmp")
 require("map")
 
+vim.cmd("source ~/.config/nvim/vim/git.vim")
+vim.cmd("source ~/.config/nvim/vim/toggle.vim")
+vim.cmd("source ~/.config/nvim/vim/colors.vim")
 vim.cmd("source ~/.config/nvim/vim/colors.vim")
