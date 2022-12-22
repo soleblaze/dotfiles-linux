352df757c3fdca4fb464c5efe0febb5bf766ffd0
Soleblaze <soleblaze@skyshadows.net>
2022-12-21 19:16:05 -0600

    Add zsh configs

---
+++ zsh/alias.zsh
@@ -0,0 +1,58 @@
+#!/bin/zsh
+
+# Override Commands
+alias cp="cp -i --reflink=auto"
+alias dosbox=dosbox -conf "$XDG_CONFIG_HOME"/dosbox/dosbox.conf
+alias free="free -h"
+alias grep="grep --color -i"
+alias ls="ls --color=always --group-directories-first"
+alias ping='prettyping --nolegend'
+alias vi=nvim
+alias wget=wget --hsts-file="$HOME/.local/share/wget-hsts"
+
+# BTRFS DF alias
+alias bdf="btrfs filesystem df"
+
+# Ripgrep Aliases
+alias rg="batgrep -S"
+alias rgi="batgrep -i"
+alias rgs="batgrep -s"
+
+# Other Aliases
+alias ndu="ncdu --color dark -rr -x --exclude .git --exclude node_modules"
+alias open="xdg-open"
+alias sih="sudo -i -H"
+alias svi="sudo -e"
+alias sz="source ~/.zshrc"
+alias xd="xdg-mime default"
+alias xqd="xdg-mime query default"
+alias xqf="xdg-mime query filetype"
+
+# bat aliases
+alias cat='bat -p --pager=never'
+alias catl='bat --pager=never'
+alias catp='bat'
+
+# ssh aliases
+alias scpnh='scp -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null"'
+alias sshnh='ssh -o "StrictHostKeyChecking no" -o "UserKnownHostsFile /dev/null"'
+
+# dot aliases
+alias -g ...="../.."
+alias -g ....="../../.."
+alias -g .....="../../../.."
+
+# archlinux aliases
+alias pac="sudo pacman -S"
+alias pacq="pacman -Q"
+alias pacr="sudo pacman -Rs"
+alias pacs="pacman -Ss"
+alias pacsy="sudo pacman -Sy"
+alias pacsyu="sudo pacman -Syu"
+alias pacu="sudo pacman -U"
+alias ya="yay -S"
+alias yas="yay -Ss"
+alias yau="yay -Su"
+
+# BTRFS DF alias
+alias bdf="btrfs filesystem df"
