c986069a5d77dd7b9d8c57f1700e53f8a9d0a7e3
Soleblaze <soleblaze@skyshadows.net>
2022-12-22 23:34:04 -0600

    Fix error when LspZeroFormat isnt available

--- nvim/lua/toggle-autoformat.lua
+++ nvim/lua/toggle-autoformat.lua
@@ -1,3 +1,3 @@
 vim.g.autoFormat = 1
-vim.cmd("autocmd BufWritePre * if get(g:, 'autoFormat', 1) | exe 'LspZeroFormat' | endif")
+vim.cmd("autocmd BufWritePre * if get(g:, 'autoFormat', 1) | if exists(':LspZeroFormat') | exe 'LspZeroFormat' | endif | endif")
 vim.cmd("nnoremap <F6> :let g:autoFormat = !get(g:, 'autoFormat', 1)<cr>")
