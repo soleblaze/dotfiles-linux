358b5f5cea6da22a0e0e69a3d55833a95538720e
Soleblaze <soleblaze@skyshadows.net>
2023-01-25 09:02:17 -0600

    Add doing aliases

---
+++ zsh/doing.zsh
@@ -0,0 +1,10 @@
+#!/bin/zsh
+
+# Doing Aliases
+# https://github.com/ttscoff/doing
+alias dn="doing now"
+alias ddone="doing done"
+alias dl="doing last"
+alias dnext="doing done; doing now"
+alias dt="doing today --totals | colout '^([ \d:apm]+) ?([>:]) (.*)' green,black,white"
+alias dy="doing yesterday --totals | colout '^([ \d:apm]+) ?([>:]) (.*)' green,black,white"
